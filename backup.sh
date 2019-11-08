f60390e3af99988a2854cc74b6563ca766749dd8
artas182x <artas182x@gmail.com>
2019-11-08 11:37:51 +0100

    Add start and backup script

---
+++ backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+localManifestBranch=$(git branch | grep \* | cut -d ' ' -f2)
+set -e
+
+function commit_and_push() {
+	local dir="$1"
+	cd "$dir" 
+	git checkout -b $localManifestBranch 2>/dev/null  || true
+	backupString="Backup $(date +%Y-%m-%d-%H:%M)"
+	find . -name '.DS_Store' -type f -delete && git add -A
+	git commit -m "${backupString}"
+	git push --force
+}
+
+
+function make_backup() {
+	local dbbackupname="db_$(date +%Y-%m-%d-%H:%M).tar.gz"
+	local psbackupname="ps_$(date +%Y-%m-%d-%H:%M).tar.gz"
+	mkdir -p backup
+	cd mariadb
+	tar -czf "../backup/${dbbackupname}" .
+	cd ../prestashop
+	tar -czf "../backup/${psbackupname}" .
+
+
+	cd ../backup
+
+	rm -f mariadb.tar.gz
+	rm -f prestashop.tar.gz
+
+	ln -s "${dbbackupname}" mariadb.tar.gz
+	ln -s "${psbackupname}" prestashop.tar.gz
+
+	cd ..
+}
+
+docker-compose stop || true
+
+make_backup
+commit_and_push backup
+
+docker-compose start || true
---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+localManifestBranch=$(git branch | grep \* | cut -d ' ' -f2)
+set -e
+
+function clone_or_checkout() {
+    local dir="$1"
+    local repo="$2"
+
+    if [[ -d "$dir" ]];then
+        (
+            cd "$dir"
+            git fetch
+            git reset --hard
+            git pull --recurse-submodules
+        )
+    else
+        git clone --recursive git@github.com:WitaminoweMisiaki/"$repo" "$dir" -b "$localManifestBranch"
+        git-crypt unlock || true
+    fi
+}
+
+function decompress_backup() {
+    rm -Rf mariadb
+    rm -Rf prestashop
+    mkdir -p mariadb
+    mkdir -p prestashop
+    tar -xf ./backup/mariadb.tar.gz -C ./mariadb
+    tar -xf ./backup/prestashop.tar.gz -C ./prestashop
+}
+
+
+clone_or_checkout $PWD PrestaShop-Docker
+
+
+decompress_backup
+
+docker-compose stop || true
+docker-compose rm -f || true
+
+docker-compose up
